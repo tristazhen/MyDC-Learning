b0d5589b784c3dd5e4b40a91e6c9eb02a2942df4
Jane <51193489+tristazhen@users.noreply.github.com>
2021-10-26 14:31:45 +0800

    Initial commit

---
+++ vue.config.js
@@ -0,0 +1,123 @@
+/**
+ * @Author: Caven
+ * @Date: 2021-01-20 10:36:49
+ */
+
+'use strict'
+const path = require('path')
+const fs = require('fs-extra')
+
+const CopywebpackPlugin = require('copy-webpack-plugin')
+const dvgis = './node_modules/@dvgis'
+
+let resolve = dir => {
+  return path.resolve(__dirname, dir)
+}
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
+  productionSourceMap: false,
+  configureWebpack: {
+    module: {
+      unknownContextCritical: false
+    },
+    performance: {
+      hints: false
+    }
+  },
+  chainWebpack: config => {
+    config.resolve.extensions
+      .add('.js')
+      .add('.vue')
+      .end()
+      .alias.set('dvgis', resolve(dvgis))
+      .end()
+
+    config.module
+      .rule('images')
+      .test(/\.(png|jpe?g|gif)(\?.*)?$/)
+      .use('url-loader')
+      .loader('url-loader')
+      .options({
+        name: 'images/[name].[ext]',
+        limit: 10000
+      })
+      .end()
+
+    config.module
+      .rule('fonts')
+      .test(/\.(eot|ttf|woff|woff2)(\?.*)?$/)
+      .use('url-loader')
+      .loader('url-loader')
+      .options({
+        name: 'fonts/[name].[ext]',
+        limit: 10000
+      })
+      .end()
+
+    config.module
+      .rule('svg')
+      .exclude.add(resolve('src/assets/svg/icons'))
+      .end()
+
+    config.module
+      .rule('icons')
+      .test(/\.svg$/)
+      .include.add(resolve('src/assets/svg/icons'))
+      .end()
+      .use('svg-sprite-loader')
+      .loader('svg-sprite-loader')
+      .options({
+        symbolId: 'icon-[name]'
+      })
+      .end()
+
+    config.plugin('copy').use(CopywebpackPlugin, [
+      [
+        {
+          from: path.join(__dirname, 'public'),
+          to: path.join(__dirname, 'dist'),
+          ignore: ['index.html']
+        },
+        {
+          from: path.join(dvgis, 'dc-sdk/dist/resources'),
+          to: path.join(__dirname, 'dist', 'libs/dc-sdk/resources')
+        }
+      ]
+    ])
+  },
+  pluginOptions: {
+    electronBuilder: {
+      nodeIntegration: true,
+      chainWebpackMainProcess: config => {
+        let outputDir = 'dist_electron/bundled'
+        fs.removeSync(path.join(__dirname, outputDir, 'Assets'))
+        fs.removeSync(path.join(__dirname, outputDir, 'Workers'))
+        fs.removeSync(path.join(__dirname, outputDir, 'ThirdParty'))
+        config.plugin('copy').use(CopywebpackPlugin, [
+          [
+            {
+              from: path.join(__dirname, 'public'),
+              to: path.join(__dirname, outputDir),
+              ignore: ['index.html']
+            },
+            {
+              from: path.join(dvgis, 'dc-sdk/dist/resources'),
+              to: path.join(__dirname, outputDir, 'libs/dc-sdk/resources')
+            }
+          ]
+        ])
+      },
+      chainWebpackRendererProcess: config => {
+        config.plugin('define').tap(args => {
+          const env = args[0]['process.env']
+          for (let key in env) {
+            args[0][`process.env.${key}`] = env[key]
+          }
+          delete args[0]['process.env']
+          return args
+        })
+      }
+    }
+  }
+}
